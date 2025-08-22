547f9b5912daabc75b55f56221d13767ca5e94be
Ubuntu <ubuntu@ip-172-31-93-97.ec2.internal>
2025-08-22 09:40:59 +0000

    comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+// src/App.js
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [visits, setVisits] = useState(0);
+
+  const handleClick = () => {
+    setVisits(visits + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My React App!</h1>
+        <p>Button clicked {visits} times</p>
+        <button onClick={handleClick}>Click Me!</button>
       </header>
     </div>
   );
 }
 
 export default App;
+
